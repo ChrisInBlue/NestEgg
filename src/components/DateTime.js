8a2968086d8bb15d586654fb8cb901592108cf7b
ChrisInBlue <chris@bluelightsoftware.co.za>
2024-04-05 11:46:57 +0200

    custom styles setup
    setting up of custom styling themes & resets

---
+++ src/components/DateTime.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+const DateTime = () => {
+  const [currentTime, setCurrentTime] = useState({
+    day: '',
+    date: '',
+    time: '',
+  });
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      const now = new Date();
+      const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'short' });
+      const date = `${now.getDate()} ${now.toLocaleString('en-US', { month: 'short' })} ${now.getFullYear()}`;
+      const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' });
+
+      setCurrentTime({ day: dayOfWeek, date, time });
+    };
+
+    updateDateTime();
+    const intervalId = setInterval(updateDateTime, 20000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className='date-time-box'>
+      <h2 id='day'>{currentTime.day}</h2>
+      <h3 id='date'>{currentTime.date}</h3>
+      <h4 id='time'>{currentTime.time}</h4>
+    </div>
+  );
+};
+
+export default DateTime;
